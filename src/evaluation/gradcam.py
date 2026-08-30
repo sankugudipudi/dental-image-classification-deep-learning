7b24b5e474c19bd91a92d78b87a88ac4c958a6d9
sankugudipudi <sankugudipudi7093@gmail.com>
2026-08-30 18:48:03 +0530

    feat: add src/evaluation/gradcam.py

---
+++ src/evaluation/gradcam.py
@@ -0,0 +1,27 @@
+"""
+Grad-CAM: Gradient-Weighted Class Activation Mapping for Dental X-Rays
+"""
+import tensorflow as tf
+import numpy as np
+import cv2
+
+def generate_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
